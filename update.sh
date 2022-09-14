9403f23d037e13f5555d2948f7a695c86c4fa009
A1337Xyz <blindwizard@tutanota.com>
2022-09-14 14:25:37 -0300

    update configs

--- update.sh
+++ update.sh
@@ -6,10 +6,9 @@ find ~/.local/src/config -mindepth 2 -type f | while read -r old
 do
     new=${old/local\/src\//}
     [ -e "$new" ] || continue
-    if ! diff --color=always "$old" "$new";then
-        printf "\nupdate '%s' (y/n)? " "${old##*/}"
-        read ask </dev/tty
-        [ "${ask,,}" = "y" ] && cp -v "$new" "$old"
-        echo -e "\r-------------------------------"
-    fi
+    diff --color=always "$old" "$new" || cp -vi "$new" "$old" </dev/tty
 done
+
+old=~/.local/src/config/bashrc
+new=~/.bashrc
+diff --color=always "$old" "$new" || cp -vi "$new" "$old"
