22fb35490cd077553c5553dfd9dc1375e205a374
A1337Xyz <blindwizard@tutanota.com>
2022-09-14 14:12:31 -0300

    update update.sh

--- update.sh
+++ update.sh
@@ -4,12 +4,12 @@ set -eo pipefail
 
 find ~/.local/src/config -mindepth 2 -type f | while read -r old
 do
-    new=$(find ~/.config -maxdepth 4 -type f -name "${old##*/}")
-    [ -f "$new" ] || continue
+    new=${old/local\/src\//}
+    [ -e "$new" ] || continue
     if ! diff --color=always "$old" "$new";then
         printf "\nupdate '%s' (y/n)? " "${old##*/}"
         read ask </dev/tty
         [ "${ask,,}" = "y" ] && cp -v "$new" "$old"
-        echo -e "\r-----------------------------\n"
+        echo -e "\r-------------------------------"
     fi
 done
