22620f1f0de4c9ba5b7af8e2d451c30a89d6ca04
A1337Xyz <blindwizard@tutanota.com>
2022-12-15 13:09:02 -0300

    2022.12.15

--- conky/functions.lua
+++ conky/functions.lua
@@ -1,8 +1,14 @@
 function conky_rjust(n, s)
-    s = conky_parse(s)
+    local s = conky_parse(s)
     return string.format('%' .. n .. 's', s)
 end
 function conky_ljust(n, s)
-    s = conky_parse(s)
+    local s = conky_parse(s)
     return string.format('%-' .. n .. 's' , s)
+end
+function conky_power_draw()
+    local f = io.open("/sys/class/power_supply/BAT0/power_now", "r")
+    local v = tonumber(f:read())
+    f:close()
+    return string.format("%.2f W", v * 10^-6)
 end
