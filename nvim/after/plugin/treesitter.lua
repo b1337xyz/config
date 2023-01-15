07d5daa07c7089bba160c2ec98bdc608f212fc04
A1337Xyz <blindwizard@tutanota.com>
2023-01-15 11:34:43 -0300

    2023.01.15

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -2,10 +2,10 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = { "python", "bash" },
   sync_install = false,
   auto_install = true,
-  ignore_install = { },
+  ignore_install = {},
   highlight = {
     enable = true,
-    disable = { },
+    disable = {},
     additional_vim_regex_highlighting = false,
   },
 }
