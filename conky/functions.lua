42436e90f81c1fd9cce37f7ab94e0c2c4265cbac
A1337Xyz <blindwizard@tutanota.com>
2023-01-22 19:51:49 -0300

    2023.01.22

--- conky/functions.lua
+++ conky/functions.lua
@@ -12,3 +12,20 @@ function conky_power_draw()
     f:close()
     return string.format("%.2f W", v * 10^-6)
 end
+-- function file_exists(name)
+--    local f=io.open(name, "r")
+--    if f ~= nil then io.close(f) return true else return false end
+-- end
+-- function conky_cover_art()
+--     local file = conky_parse("${mpd_file}")
+--     local music_dir = os.getenv('HOME') .. "/Music"
+--     local cache = os.getenv('HOME') .. '/.cache/thumbnails/mpc'
+--     local image = cache .. music_dir .. "/" .. file .. ".jpg"
+--     local dst = '/tmp/conky.mpd.jpg'
+--     if file_exists(image) then
+--         os.execute('cp "' .. image .. '" ' .. dst)
+--     else
+--         os.execute('rm ' .. dst)
+--     end
+--     return string.format("${image %s -p 0,0 -s 260x260 -n}", dst)
+-- end
