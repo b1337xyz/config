25a3c45529d7a29f63beafb6ab2c2393aec38b40
A1337Xyz <blindwizard@tutanota.com>
2023-08-02 13:31:56 -0300

    2023.08.02

--- qutebrowser/qbfetch.py
+++ qutebrowser/qbfetch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from qutebrowser.api import cmdutils, message
+from qutebrowser.api import cmdutils
 from qutebrowser.browser.qutescheme import add_handler
 from qutebrowser.utils import objreg, version as vs
 # from qutebrowser import __file__ as qtfile
@@ -8,7 +8,6 @@ from qutebrowser.qt.core import QUrl
 # import traceback
 # import os
 
-HTML = '/tmp/qbfetch.html'
 html_head = '''<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -41,76 +40,68 @@ html_head = '''<!DOCTYPE html>
 html_tail = '</div></body></html>'
 
 
-# @cmdutils.register()
-# @cmdutils.argument('win_id', value=cmdutils.Value.win_id)
 @add_handler('qbfetch')
 def qbfetch(_url: QUrl) -> None:
-    """
-        Display information from qute://version in a "pleasant" way
-    """
+    """ Handler for qute://qbfetch. """
+    lines = [('qutebrowser v', qtver)]
+    gitver = vs._git_str()
+    if gitver is not None:
+        lines.append(("Git commit", gitver))
 
-    try:
-        lines = [('qutebrowser v', qtver)]
-        gitver = vs._git_str()
-        if gitver is not None:
-            lines.append(("Git commit", gitver))
+    lines.append(('Backend', vs._backend()))
+    # lines.append(('Qt', vs.earlyinit.qt_version()))
+    lines.append((vs.platform.python_implementation(),
+                  vs.platform.python_version()))
+    # lines.append(('PyQt', vs.PYQT_VERSION_STR))
+    # m = str(vs.machinery.INFO).split('\n')
+    # lines += [(m[0].replace(':', ''), '<br>'.join(m[1:]))]
+    lines += [s.split(': ') for s in vs._module_versions()]
+    lines.append(('pdf.js', vs._pdfjs_version()))
+    lines.append(('sqlite', vs.sql.version()))
+    lines.append(('QtNetwork SSL', vs.QSslSocket.sslLibraryVersionString()
+                  if vs.QSslSocket.supportsSsl() else 'no'))
 
-        lines.append(('Backend', vs._backend()))
-        # lines.append(('Qt', vs.earlyinit.qt_version()))
-        lines.append((vs.platform.python_implementation(),
-                      vs.platform.python_version()))
-        # lines.append(('PyQt', vs.PYQT_VERSION_STR))
-        # m = str(vs.machinery.INFO).split('\n')
-        # lines += [(m[0].replace(':', ''), '<br>'.join(m[1:]))]
-        lines += [s.split(': ') for s in vs._module_versions()]
-        lines.append(('pdf.js', vs._pdfjs_version()))
-        lines.append(('sqlite', vs.sql.version()))
-        lines.append(('QtNetwork SSL', vs.QSslSocket.sslLibraryVersionString()
-                      if vs.QSslSocket.supportsSsl() else 'no'))
+    if vs.objects.qapp:
+        style = vs.objects.qapp.style()
+        metaobj = style.metaObject()
+        if metaobj:
+            lines.append(('Style', metaobj.className()))
+        # lines.append(('Platform plugin', vs.objects.qapp.platformName()))
+        lines.append(('OpenGL', vs.opengl_info()))
 
-        if vs.objects.qapp:
-            style = vs.objects.qapp.style()
-            metaobj = style.metaObject()
-            if metaobj:
-                lines.append(('Style', metaobj.className()))
-            # lines.append(('Platform plugin', vs.objects.qapp.platformName()))
-            lines.append(('OpenGL', vs.opengl_info()))
+    lines += [('Platform', '{}, {}'.format(vs.platform.platform(),
+                                           vs.platform.architecture()[0]))]
 
-        lines += [('Platform', '{}, {}'.format(vs.platform.platform(),
-                                               vs.platform.architecture()[0]))]
+    dist = vs.distribution()
+    if dist is not None:
+        lines += [('OS', f'{dist.pretty} ({dist.parsed.name})')]
 
-        dist = vs.distribution()
-        if dist is not None:
-            lines += [('OS', f'{dist.pretty} ({dist.parsed.name})')]
+    # importpath = os.path.dirname(os.path.abspath(qtfile))
+    # lines.append(('Frozen', hasattr(sys, 'frozen')))
+    # lines.append(("Imported from", importpath))
+    # lines.append(("Using Python from", sys.executable))
+    # lines += [(name, path)
+    #           for name, path in sorted(vs._path_info().items())]
 
-        # importpath = os.path.dirname(os.path.abspath(qtfile))
-        # lines.append(('Frozen', hasattr(sys, 'frozen')))
-        # lines.append(("Imported from", importpath))
-        # lines.append(("Using Python from", sys.executable))
-        # lines += [(name, path)
-        #           for name, path in sorted(vs._path_info().items())]
+    lines += [
+        ('Autoconfig loaded', vs._autoconfig_loaded()),
+        ('Config.py', vs._config_py_loaded()),
+        ('Uptime', vs._uptime())
+    ]
 
-        lines += [
-            ('Autoconfig loaded', vs._autoconfig_loaded()),
-            ('Config.py', vs._config_py_loaded()),
-            ('Uptime', vs._uptime())
-        ]
+    out = ''
+    for k, v in lines:
+        out += f'<div><span>{k}</span><span>:</span> <span>{v}</span></div>\n'
 
-        out = ''
-        for k, v in lines:
-            out += f'<div><span>{k}</span><span>:</span> <span>{v}</span></div>\n'  # noqa: E501
+    return 'text/html', html_head + out + html_tail
 
-        return 'text/html', html_head + out + html_tail
 
-        # with open(HTML, 'w') as f:
-        #     f.write(html_head + out + html_tail)
-
-        # url = QUrl(f'file://{HTML}')
-        # tabbed_browser = objreg.get('tabbed-browser',
-        #                             scope='window',
-        #                             window=win_id)
-        # tabbed_browser.load_url(url, newtab=False)
-    except Exception:
-        # with open('qterr', 'w') as f:
-        #     traceback.print_exc(file=f)
-        message.error('Failed')
+@cmdutils.register()
+@cmdutils.argument('win_id', value=cmdutils.Value.win_id)
+def qbfetch(win_id: int):
+    """ Show version information in a "pleasant" way. """
+    url = QUrl('qute://qbfetch')
+    tabbed_browser = objreg.get('tabbed-browser',
+                                scope='window',
+                                window=win_id)
+    tabbed_browser.load_url(url, newtab=False)
