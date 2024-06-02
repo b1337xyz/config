f36613acf3ab4f370db8e0f33312177bd97acbd3
Blind Wizard <blindwizard@tutanota.com>
2024-06-02 11:52:31 -0300

    2024.06.02

--- qutebrowser/qbfetch.py
+++ qutebrowser/qbfetch.py
@@ -43,8 +43,7 @@ if 'qbfetch' not in objects.commands:
         color: #f7768e
     }''')
 
-    html_head = '''<!DOCTYPE html>
-    <html lang="en">
+    html_head = '''<!DOCTYPE html><html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
